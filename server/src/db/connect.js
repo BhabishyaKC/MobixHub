d67f8803a5ed57046ec10f67b5325ab1940134b4
BhabishyaKC <bhabishyakc19@gmail.com>
2025-06-13 01:26:33 +0545

    database setup

---
+++ server/src/db/connect.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+async function connect() {
+  const res = await mongoose.connect('mongodb://localhost:27017/mobixHubDb')
+  if (res) {
+    console.log('Connected to MongoDB')
+  }
+  
+}
+export default connect;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
-const express = require('express')
+import express from 'express'
+import connect from './db/connect.js'
 const app = express()
 const port = 8000
+
+connect()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
